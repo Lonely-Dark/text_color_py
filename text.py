3e29cf93d85f28c8a351df5b197ef0e906733cc1
Lonely Dark <42666581+Lonely-Dark@users.noreply.github.com>
2019-08-12 22:05:49 +0300

    Update and rename tekst.py to text.py

---
+++ text.py
@@ -0,0 +1,17 @@
+#Python 3.7.3
+#Make by Lonely-Dark
+
+Red = '\033[91m'
+Green = '\033[92m'
+Yellow = '\033[93m'
+Blue = '\033[94m'
+Magenta = '\033[95m'
+Cyan = '\033[96m'
+White = '\033[97m'
+Grey = '\033[90m'
+BOLD = '\033[1m'
+ITALIC = '\033[3m'
+UNDERLINE = '\033[4m'
+END = '\033[0m'
+
+print(Red + "This is text is red" + END)
